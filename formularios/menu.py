1bbd255f51a21c19ad048aa0aa75bfe156ebb6e6
Francisco José Rodríguez Bogado <pacoqueen@gmail.com>
2012-05-31 20:03:39 +0200

    Cambio de versión.

--- formularios/menu.py
+++ formularios/menu.py
@@ -71,7 +71,7 @@ if path_framework not in sys.path:
     sys.path.append(path_framework)
 from configuracion import ConfigConexion
 
-__version__ = '3.9.4 (build ~3392) C14r4'
+__version__ = '3.9.5 (build ~3392) C14r4'
 __version_info__ = tuple(
     [int(num) for num in __version__.split()[0].split('.')] + 
     [txt.replace("(", "").replace(")", "") for txt in __version__.split()[1:]]
