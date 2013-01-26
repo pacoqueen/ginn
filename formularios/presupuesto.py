8247b9f82557e6ecd240634c3605fcdf44685f05
Francisco José Rodríguez Bogado <frbogado@geotexan.com>
2013-01-26 20:52:24 +0100

    Primeras pruebas presupuesto anual.

---
+++ formularios/presupuesto.py
@@ -0,0 +1,193 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+###############################################################################
+# Copyright (C) 2005-2008  Francisco José Rodríguez Bogado,                   #
+#                          (pacoqueen@users.sourceforge.net                   #
+#                                                                             #
+# This file is part of GeotexInn.                                             #
+#                                                                             #
+# GeotexInn is free software; you can redistribute it and/or modify           #
+# it under the terms of the GNU General Public License as published by        #
+# the Free Software Foundation; either version 2 of the License, or           #
+# (at your option) any later version.                                         #
+#                                                                             #
+# GeotexInn is distributed in the hope that it will be useful,                #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of              #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               #
+# GNU General Public License for more details.                                #
+#                                                                             #
+# You should have received a copy of the GNU General Public License           #
+# along with GeotexInn; if not, write to the Free Software                    #
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA  #
+###############################################################################
+
+
+###################################################################
+## presupuesto.py - Presupuesto anual.
+###################################################################
+## Changelog:
+## 25 de enero de 2012 -> Inicio
+## 
+###################################################################
+
+import sys, os
+from ventana import Ventana
+import utils
+import pygtk
+pygtk.require('2.0')
+import gtk, gtk.glade, time, mx, mx.DateTime
+try:
+    import pclases
+    from seeker import VentanaGenerica 
+except ImportError:
+    sys.path.append(os.path.join('..', 'framework'))
+    import pclases
+    from seeker import VentanaGenerica 
+from utils import _float as float
+from ventana_progreso import VentanaProgreso
+
+
+class Presupuesto(Ventana, VentanaGenerica):
+    def __init__(self, objeto = None, usuario = None):
+        """
+        Constructor. objeto puede ser un objeto de pclases con el que
+        comenzar la ventana (en lugar del primero de la tabla, que es
+        el que se muestra por defecto).
+        """
+        self.usuario = usuario
+        self.clase = None
+        self.dic_campos = {}
+        Ventana.__init__(self, 'presupuesto.glade', objeto)
+        connections = {'b_salir/clicked': self.salir,
+                       'b_nuevo/clicked': self.nuevo,
+                       'b_borrar/clicked': self.borrar,
+                       'b_actualizar/clicked': self.actualizar_ventana,
+                       # 'b_guardar/clicked': self.guardar,
+                       'b_buscar/clicked': self.buscar,
+                      }  
+        self.add_connections(connections)
+        self.inicializar_ventana()
+        gtk.main()
+
+    def es_diferente(self):
+        """
+        Devuelve True si algún valor en ventana difiere de 
+        los del objeto.
+        """
+        if self.objeto == None:
+            igual = True
+        else:
+            igual = self.objeto != None
+            for colname in self.dic_campos:
+                col = self.clase._SO_columnDict[colname]
+                try:
+                    valor_ventana = self.leer_valor(col, self.dic_campos[colname])
+                except (ValueError, mx.DateTime.RangeError, TypeError):
+                    igual = False
+                valor_objeto = getattr(self.objeto, col.name)
+                if isinstance(col, pclases.SODateCol):
+                    valor_objeto = utils.abs_mxfecha(valor_objeto)
+                igual = igual and (valor_ventana == valor_objeto)
+                if not igual:
+                    break
+        return not igual
+    
+    def inicializar_ventana(self):
+        """
+        Inicializa los controles de la ventana, estableciendo sus
+        valores por defecto, deshabilitando los innecesarios,
+        rellenando los combos, formateando el TreeView -si lo hay-...
+        """
+        # Inicialmente no se muestra NADA. Sólo se le deja al
+        # usuario la opción de buscar o crear nuevo.
+        self.activar_widgets(False)
+        self.wids['b_actualizar'].set_sensitive(True)
+        self.wids['b_guardar'].set_sensitive(False)
+        self.wids['b_nuevo'].set_sensitive(True)
+        self.wids['b_buscar'].set_sensitive(True)
+        # Inicialización del resto de widgets:
+        cols = [('Concepto', 'gobject.TYPE_STRING', False, True, True, None)]
+        for m in range(12):
+            mes = mx.DateTime.localtime().month + m
+            strmes = mx.DateTime.DateTimeFrom(month = mes).strftime("%B")
+            cols += [(strmes, 'gobject.TYPE_STRING', False, True, True, None)]
+        cols += [('ID', 'gobject.TYPE_INT64', False, False, False, None)]
+                # La última columna (oculta en la Vista) siempre es el id.
+        utils.preparar_listview(self.wids['tv_datos'], cols)
+        self.wids['ventana'].resize(800, 600)
+
+    def activar_widgets(self, s, chequear_permisos = True):
+        """
+        Activa o desactiva (sensitive=True/False) todos 
+        los widgets de la ventana que dependan del 
+        objeto mostrado.
+        Entrada: s debe ser True o False. En todo caso
+        se evaluará como boolean.
+        """
+        if self.objeto == None:
+            s = False
+        ws = []
+        for w in ws:
+            try:
+                self.wids[w].set_sensitive(s)
+            except Exception, msg:
+                print "Widget problemático:", w, "Excepción:", msg
+                import traceback
+                traceback.print_last()
+        if chequear_permisos:
+            self.check_permisos(nombre_fichero_ventana = "presupuesto.py")
+
+    def rellenar_widgets(self):
+        """
+        Introduce la información del objeto actual
+        en los widgets.
+        No se chequea que sea != None, así que
+        hay que tener cuidado de no llamar a 
+        esta función en ese caso.
+        """
+        self.rellenar_tabla()
+
+    def rellenar_tabla(self):
+        vpro = VentanaProgreso(padre = self.wids['ventana'])
+        vpro.mostrar()
+        model = self.wids['tv_datos'].get_model()
+        model.clear()
+        for m in range(12):
+            model.append((m, "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", None)) 
+            vpro.set_valor(m/12.0, 
+                           mx.DateTime.DateTimeFrom(month = m).strftime("%B"))
+            # PORASQUI: XXX: Rellenar filas y columnas 
+        vpro.ocultar()
+            
+    def nuevo(self, widget):
+        """
+        Función callback del botón b_nuevo.
+        Pide los datos básicos para crear un nuevo objeto.
+        Una vez insertado en la BD hay que hacerlo activo
+        en la ventana para que puedan ser editados el resto
+        de campos que no se hayan pedido aquí.
+        """
+        # TODO: : Nuevo concepto
+        pass
+
+    def buscar(self, widget):
+        """
+        Muestra una ventana de búsqueda y a continuación los
+        resultados. El objeto seleccionado se hará activo
+        en la ventana a no ser que se pulse en Cancelar en
+        la ventana de resultados.
+        """
+        # TODO: : Buscar dentro de todas las filas un texto tecleado y pasarle el foco o algo.
+        pass
+
+    def borrar(self, widget):
+        """
+        Elimina los datos de la fila seleccionada o la fila completa.
+        """
+        # TODO: 
+        pass
+
+if __name__ == "__main__":
+    p = Presupuesto()
+
