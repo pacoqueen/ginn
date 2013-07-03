a4382fac2e3f9b6e45670976ea4c12120fbeefdb
Francisco José Rodríguez Bogado <frbogado@geotexan.com>
2013-07-03 11:48:13 +0200

    Tema estándar de versión en producción.

--- ginn/framework/pclases/superfacturaventa.py
+++ ginn/framework/pclases/superfacturaventa.py
@@ -1,6 +1,31 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+###############################################################################
+# Copyright (C) 2005-2013  Francisco José Rodríguez Bogado                    #
+#                          <frbogado@geotexan.com>                            #
+#                                                                             #
+# This file is part of GeotexInn.                                             #
+#                                                                             #
+# GeotexInn is free software; you can redistribute it and/or modify           #
+# it under the terms of the GNU General Public License as published by        #
+# the Free Software Foundation; either version 2 of the License, or           #
+# (at your option) any later version.                                         #
+#                                                                             #
+# GeotexInn is distributed in the hope that it will be useful,                #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of              #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               #
+# GNU General Public License for more details.                                #
+#                                                                             #
+# You should have received a copy of the GNU General Public License           #
+# along with GeotexInn; if not, write to the Free Software                    #
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA  #
+###############################################################################
+
+#########################################################################
+## superfacturaventa.py - Clase base para todas las facturas de venta. ##
+#########################################################################
+
 from . import * 
 
 class SuperFacturaVenta:
