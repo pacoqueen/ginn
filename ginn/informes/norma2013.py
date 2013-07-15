9a6be5de5c60d66ff62ddf9e257e9395a16f301a
Francisco José Rodríguez Bogado <frbogado@geotexan.com>
2013-07-15 12:51:17 +0200

    Letra más legible en producto y código. (jmadrid).

--- ginn/informes/norma2013.py
+++ ginn/informes/norma2013.py
@@ -17,10 +17,11 @@ from framework import pclases
 from collections import defaultdict
 from formularios import utils
 
-def etiqueta_rollos_norma13(rollos, mostrar_marcado = True):
+def etiqueta_rollos_norma13(rollos, mostrar_marcado = True, lang = "es"):
     """
     Construye una etiqueta por cada objeto rollo recibido y las devuelve 
     en un solo PDF.
+    Si lang = "en", etiqueta en inglés. Si "es", en castellano.
     """
     # Voy a tratar de reescribir esto regla en mano a ver si consigo 
     # cuadrarlo bien en la etiquetadora GEMINI.
@@ -29,11 +30,11 @@ def etiqueta_rollos_norma13(rollos, mostrar_marcado = True):
 
     # Creo la hoja
     nomarchivo = os.path.join(gettempdir(),
-        "etiq_norma13_%s.pdf" % give_me_the_name_baby())
+        "etiq_norma13_%s_%s.pdf" % (lang, give_me_the_name_baby()))
     c = canvas.Canvas(nomarchivo, pagesize = (ancho, alto))
     
     # Medidas:
-    logo = (3.8 * cm, 2.8 * cm)
+    logo = (3.8 * cm * 0.75, 2.8 * cm * 0.75)
     margen = 0.1 * cm
     marcado = (((ancho - logo[0]) / 2) - margen, (alto - margen - logo[1] - 2))
     
@@ -62,11 +63,14 @@ def etiqueta_rollos_norma13(rollos, mostrar_marcado = True):
              "16 codigo": "Partida: %d Rollo: %d", 
              "17 caracteristicas": "Gramaje: %d g/m² Ancho: %s m Largo: %d m" 
             }
+    if lang == "en":
+        for k in _data:
+            _data[k] = helene_laanest(_data[k])
     estilos = defaultdict(lambda: ("Helvetica", 9)) # Helvética a 9 por defecto
-    estilos["02 fabricado_por"] = ("Helvetica-Bold", 10)
-    estilos["12 producto"] = ("Helvetica-Bold", 9)
-    estilos["16 codigo"] = ("Helvetica-Bold", 9)
-    estilos["17 caracteristicas"] = ("Helvetica-Bold", 9)
+    estilos["02 fabricado_por"] = ("Helvetica-Bold", 11)
+    estilos["12 producto"] = ("Helvetica-Bold", 17)
+    estilos["16 codigo"] = ("Helvetica-Bold", 17)
+    estilos["17 caracteristicas"] = ("Helvetica-Bold", 13)
     data = {}
     # Datos de la BD dependientes del rollo
     for rollo in rollos:
@@ -114,7 +118,10 @@ def etiqueta_rollos_norma13(rollos, mostrar_marcado = True):
         data["08 dni"] = producto.dni
         data["12 producto"] = producto.nombre
         if producto.uso:
-            data["14 uso"] = _data["14 uso"] % producto.uso
+            if lang == "en":
+                data["14 uso"] = _data["14 uso"] % helene_laanest(producto.uso)
+            else:
+                data["14 uso"] = _data["14 uso"] % producto.uso
         else:
             data["14 uso"] = ""
     #   3.- Rollo
@@ -180,158 +187,22 @@ def helene_laanest(texto):
             = "Drainage, filtration, reinforcement, separation and protection"
     translate_table["Fibra de polipropileno virgen embolsada en papel hidrosoluble para su uso como aditivo del hormigón"] \
             = "100% polypropylene fibers in water-soluble paper bags used like an additive for concrete"
+    translate_table["Fabricado por: %s"] = "Manufactured by: %s"
+    translate_table["Tfno: +34 %s, %s"] = "Phone: +34 %s, %s"
+    translate_table["De EN13249:2001 a EN13257:2001"] = "From EN13249:2001 to EN13257:2001"
+    translate_table["Geotextil no tejido de polipropileno 100% virgen"] = "Nonwoven geotextile of 100% polypropylene fibres."
+    translate_table["Uso: %s"] = "Use: %s"
+    translate_table["Partida: %d Rollo: %d"] = "Batch: %d Roll: %d"
+    translate_table["Gramaje: %d g/m² Ancho: %s m Largo: %d m"] = "Mass per area: %d g/m² Width: %s m Length: %d m" 
     return translate_table[texto]
+
 
 def etiqueta_rollos_norma13_en(rollos, mostrar_marcado = True):
     """
     Construye una etiqueta por cada objeto rollo recibido y las devuelve 
     en un solo PDF. Etiqueta en inglés.
     """
-    # Voy a tratar de reescribir esto regla en mano a ver si consigo 
-    # cuadrarlo bien en la etiquetadora GEMINI.
-    alto = 12.55 * cm
-    ancho = 8.4 * cm
-
-    # Creo la hoja
-    nomarchivo = os.path.join(gettempdir(),
-        "etiq_norma13_en%s.pdf" % give_me_the_name_baby())
-    c = canvas.Canvas(nomarchivo, pagesize = (ancho, alto))
-    
-    # Medidas:
-    logo = (3.8 * cm, 2.8 * cm)
-    margen = 0.1 * cm
-    marcado = (((ancho - logo[0]) / 2) - margen, (alto - margen - logo[1] - 2))
-    
-    # Imágenes:
-    logo_marcado = os.path.abspath(os.path.join(os.path.dirname(__file__), 
-                                   "..", "imagenes", "CE.png"))
-
-    # Datos fijos:
-    _data = {# "00 logo_marcado": None, 
-             "01 texto_marcado": "1035",    # Fijo
-             "02 fabricado_por": "Manufactured by: %s", 
-             "03 direccion1": None, 
-             "04 direccion2": None, 
-             "05 telefono": "Phone: +34 %s, %s", 
-             "06 año_certif": None, 
-             "07 blanco1": "",      # Separador
-             "08 dni": None, 
-             "09 iso1": "From EN13249:2001 to EN13257:2001",  # Fijo
-             "10 iso2": "EN13265:2001",     # Fijo
-             "11 blanco2": "",      # Separador
-             "12 producto": None, 
-             "13 descripcion": 
-                 "Nonwoven geotextile of 100% polypropylene fibres.", 
-             "14 uso": "Use: %s", 
-             "15 blanco3": "",      # Separador 
-             "16 codigo": "Batch: %d Roll: %d", 
-             "17 caracteristicas": 
-                "Mass per area: %d g/m² Width: %s m Length: %d m" 
-            }
-    estilos = defaultdict(lambda: ("Helvetica", 9)) # Helvética a 9 por defecto
-    estilos["02 fabricado_por"] = ("Helvetica-Bold", 10)
-    estilos["12 producto"] = ("Helvetica-Bold", 9)
-    estilos["16 codigo"] = ("Helvetica-Bold", 9)
-    estilos["17 caracteristicas"] = ("Helvetica-Bold", 9)
-    data = {}
-    # Datos de la BD dependientes del rollo
-    for rollo in rollos:
-        # 0.- ¿En qué formato viene? Si es el antiguo (datos en diccionario) 
-        #     me quedo con el objeto de pclases en sí.
-        if isinstance(rollo, dict):
-            productoVenta = rollo['productoVenta']
-            numpartida = utils.parse_numero(rollo['partida'])
-            numrollo = int(rollo['nrollo'])
-        else:
-            productoVenta = rollo.productoVenta
-            numpartida = rollo.partida.numpartida
-            numrollo = rollo.numrollo
-        #   1.- Empresa
-        try:
-            empresa = pclases.DatosDeLaEmpresa.select()[0]
-            data["02 fabricado_por"] = _data["02 fabricado_por"] % (
-                                                                empresa.nombre)
-            # Si hay distribuidor, este texto cambia.
-            distribuidor = rollo.productoVenta.camposEspecificosRollo.cliente
-            if distribuidor:
-                data["03 direccion1"] = "Distributed by: %s" % (
-                                                        distribuidor.nombre)
-                data["04 direccion2"] = distribuidor.get_direccion_completa()
-                data["05 telefono"] = _data["05 telefono"] % (
-                        distribuidor.telefono, distribuidor.email)
-            else:   # Sigo con los datos de "propia empresa". Distribuyo yo.
-                data["03 direccion1"] = empresa.direccion + ", " + empresa.cp
-                data["04 direccion2"] = ", ".join((empresa.ciudad, 
-                                                   empresa.provincia, 
-                                                   empresa.pais))
-                data["05 telefono"] = _data["05 telefono"] % (empresa.telefono,
-                                                              empresa.email)
-        except IndexError:
-            data["02 fabricado_por"] = ""
-            data["03 direccion1"] = ""
-            data["04 direccion2"] = ""
-            data["05 telefono"] = ""
-    #   2.- Producto
-        producto = productoVenta
-        if producto.annoCertificacion != None:
-            data["06 año_certif"] = "%02d" % producto.annoCertificacion
-        else:
-            data["06 año_certif"] = ""
-        data["08 dni"] = producto.dni
-        data["12 producto"] = producto.nombre
-        if producto.uso:
-            data["14 uso"] = _data["14 uso"] % helene_laanest(producto.uso)
-        else:
-            data["14 uso"] = ""
-    #   3.- Rollo
-        data["16 codigo"] = _data["16 codigo"] % (numpartida, 
-                                                  numrollo)
-        data["17 caracteristicas"] = _data["17 caracteristicas"] % (
-            producto.camposEspecificosRollo.gramos, 
-            float2str(producto.camposEspecificosRollo.ancho, 
-                      autodec = True, separador_decimales = "."), 
-            producto.camposEspecificosRollo.metrosLineales)
-
-        rectangulo(c, (margen, margen),
-                      (ancho - margen, alto - margen))
-        if mostrar_marcado: 
-            c.drawImage(logo_marcado, 
-                        marcado[0], 
-                        marcado[1],
-                        width = logo[0], height = logo[1])
-        else:
-            data["01 texto_marcado"] = ""
-        lineas = _data.keys()
-        lineas.sort()
-        # Posición y estilo de la primera línea.
-        tamfuente = estilos[lineas[0]][1]
-        y = alto - logo[1] - 0.1 * cm - tamfuente
-        # ¿Cuánto me desplazaré de línea a línea?
-        offset_y = (y - margen) / len(lineas)
-        for linea in lineas:
-            try:
-                dato = data[linea]
-            except KeyError: # Si no está en los valores asignados, busco en 
-                             # los originales. Deben ser datos fijos.
-                dato = _data[linea]
-            if dato is None:
-                dato = ""
-            c.setFont(*estilos[linea])
-            #c.drawCentredString((ancho / 2), 
-            #                    y, 
-            #                    escribe(dato))
-            el_encogedor_de_fuentes_de_doraemon(c, 
-                                                fuente = estilos[linea][0], 
-                                                tamannoini = estilos[linea][1],
-                                                xini = margen, 
-                                                xfin = ancho - margen, 
-                                                y = y, 
-                                                texto = dato, 
-                                                alineacion = 0)
-            y -= offset_y
-        c.showPage()
-    c.save()
-    return nomarchivo
+    return etiqueta_rollos_norma13(rollos, mostrar_marcado, lang = "en")
 
 
 if __name__ == "__main__":
@@ -340,6 +211,6 @@ if __name__ == "__main__":
         if "GEOTESAN" in p.nombre and " 21 " in p.descripcion and p.articulos:
             rollos = [a.rollo for a in p.articulos[:2]]
             break
-    abrir_pdf(etiqueta_rollos_norma13(rollos))
-    abrir_pdf(etiqueta_rollos_norma13_en(rollos, False))
+    abrir_pdf(etiqueta_rollos_norma13(rollos, False))
+    abrir_pdf(etiqueta_rollos_norma13_en(rollos))
 
