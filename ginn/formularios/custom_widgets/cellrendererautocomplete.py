ca95be33562621072a09c57eb5f9b99d93fe1b29
Francisco José Rodríguez Bogado <frbogado@geotexan.com>
2013-09-03 17:19:00 +0200

    USABILIDAD: Al editar la descripción de un producto ya no se borra como antes. Ahora comienza a editar por el mismo texto que tenía y vuelve a escribir lo mismo si cancela dejándolo en blanco.

--- ginn/formularios/custom_widgets/cellrendererautocomplete.py
+++ ginn/formularios/custom_widgets/cellrendererautocomplete.py
@@ -19,14 +19,19 @@ class CellRendererAutoComplete(gtk.CellRendererText):
         if not self.get_property('editable'):
             return
         entry = gtk.Entry()
-        entry.set_text("")
+        descripcion_anterior = treeview.get_model()[path][1]
+        entry.set_text(descripcion_anterior)
         entry.set_completion(self.completion)
-        entry.connect('editing-done', self.editing_done, path)
+        entry.connect('editing-done', self.editing_done, path, 
+                                                         descripcion_anterior)
         entry.show()
         entry.grab_focus()
         return entry
 
-    def editing_done(self, entry, path):
-        self.emit('edited', path, entry.get_text())
+    def editing_done(self, entry, path, texto_anterior = ""):
+        texto = entry.get_text()
+        if not texto:
+            texto = texto_anterior
+        self.emit('edited', path, texto)
 
 
