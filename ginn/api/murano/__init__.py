66af65e797d4c57a65a3a2bfd00a76eb23bd0db6
Francisco José Rodríguez Bogado <pacoqueen@gmail.com>
2015-10-29 15:27:12 +0100

    Pruebas de interfaz puente ginn<-->Murano (API CRUD)

---
+++ ginn/api/murano/__init__.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__doc__ = """
+API de conexión con Sage Murano
+===============================
+
+Proporciona un interfaz CRUD de intercambio de datos con Sage Murano.
+
+CREATE
+------
+* Creación de balas de fibra (A, B y C).
+* Creación de bigbags de fibra (A).
+* Creación de rollos de geotextiles (A, B y C).
+* Creación de palés y cajas de fibra de cemento embolsada (A).
+
+READ
+----
+* Consulta de datos de proveedores.
+* Consulta de datos de clientes.
+* Consulta de datos de productos.
+
+UPDATE
+------
+* Modificación de calidad de un artículo.
+* Cambio de producto de un artículo.
+* Incremento o decremento del stock de un producto.
+
+DELETE
+------
+* Eliminación de artículos.
+
+"""
