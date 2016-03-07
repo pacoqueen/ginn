de9da2d9ed86e53444e5ea9ea630308384a039b1
Francisco José Rodríguez Bogado <pacoqueen@gmail.com>
2016-03-07 16:54:16 +0100

    TYPO: Corregido error en selección precio por familia. [API Murano]

--- ginn/api/murano/ops.py
+++ ginn/api/murano/ops.py
@@ -271,7 +271,7 @@ def buscar_precio_coste_familia_murano(cod_familia):
     """
     c = Connection()
     SQL = r"""SELECT TOP 1 PrecioPorUnidadEspecifica
-              FROM [%s].[dbo].[Familia]
+              FROM [%s].[dbo].[Familias]
               WHERE CodigoFamilia = '%s'
                 AND CodigoSubfamilia = '***********'
                 AND CodigoEmpresa = '%d';
