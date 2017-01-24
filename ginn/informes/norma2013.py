49ad3d4f64f09d2054f6eee02c146489bae17a9a
Francisco José Rodríguez Bogado <pacoqueen@gmail.com>
2017-01-24 18:07:07 +0100

    Agregada generación de códigos QR.

--- ginn/informes/norma2013.py
+++ ginn/informes/norma2013.py
@@ -226,6 +226,15 @@ def etiqueta_rollos_norma13(rollos, mostrar_marcado=True, lang="es"):
         c.setFont("Courier-Bold", 9)
         c.drawCentredString(xcode, ycode, codigo_rollo,charSpace=0.25*cm)
         c.restoreState()
+        # Y el QR de regalo
+        from lib.pyqrcode import pyqrcode
+        bidicode = pyqrcode.create(codigo_rollo)
+        nomfichbidi = os.path.join(gettempdir(),
+                                   "bidi_%s.svg" % give_me_the_name_baby())
+        bidicode.svg(nomfichbidi)
+        from lib.svglib.svglib import svglib
+        drawing = svglib.svg2rlg(nomfichbidi)
+        drawing.drawOn(c, margen, alto - margen - 1.0*cm)
         #######################################################################
         c.showPage()
     c.save()
