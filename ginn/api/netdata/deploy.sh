44f689cd3ce4346af51bcae630e582bbb09db071
Francisco José Rodríguez Bogado <bogado@qinn.es>
2018-10-25 17:33:56 +0200

    Probando deploy

--- ginn/api/netdata/deploy.sh
+++ ginn/api/netdata/deploy.sh
@@ -10,3 +10,4 @@ BASEPATH=$(dirname $(realpath $0))
 cp -f $BASEPATH/ginn.chart.py /usr/libexec/netdata/python.d/ginn.chart.py
 cp -f $BASEPATH/ginn.conf /etc/netdata/orig/python.d/ginn.conf
 cp -f $BASEPATH/ginn.local.conf /etc/netdata/python.d/ginn.conf
+echo -e "\e[92mMódulo copiado"
