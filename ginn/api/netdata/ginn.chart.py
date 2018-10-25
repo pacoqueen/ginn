f65fbb09c36cffeb33417bf77875bf0177e9a935
Francisco José Rodríguez Bogado <bogado@qinn.es>
2018-10-25 18:29:22 +0200

    Pruebas netdata artículos: bultos y kg.

--- ginn/api/netdata/ginn.chart.py
+++ ginn/api/netdata/ginn.chart.py
@@ -16,7 +16,7 @@ from framework import pclases
 priority = 90000
 retries = 60
 
-ORDER = ['articulos', 'random']
+ORDER = ['articulos', 'produccion', 'random']
 CHARTS = {
     'random': {
         'options': [None, 'A random number', 'ordinal', 'random', 'random',
@@ -37,6 +37,19 @@ CHARTS = {
             ['rollos A'],
             ['rollos B'],
             ['rollos C']
+        ]
+    },
+    'produccion': {
+        'options': [None, 'Articulos fabricados', 'kg', 'articulos',
+                    'produccion', 'line'],
+        'lines': [
+            ['kg fibra A+B'],
+            ['kg fibra C'],
+            ['kg fibra bigbag'],
+            ['kg fibra cemento'],
+            ['kg geotextiles A'],
+            ['kg geotextiles B'],
+            ['kg geotextiles C']
         ]
     }
 }
@@ -65,22 +78,39 @@ class Service(SimpleService):
             if dimension_id not in self.charts['random']:
                 self.charts['random'].add_dimension([dimension_id])
             data[dimension_id] = self.random.randint(0, 100)
-        # Datos para el gráfico de producción
+        # Datos para el gráfico de productos terminados (almacén)
         raw = self._get_raw_data()
-        data['balas A+B'] = raw[pclases.Bala]
-        data['balas C'] = raw[pclases.BalaCable]
-        data['bigbag'] = raw[pclases.Bigbag]
-        data['cajas'] = raw[pclases.Caja]
-        data['pales'] = raw[pclases.Pale]
-        data['rollos A'] = raw[pclases.Rollo]
-        data['rollos B'] = raw[pclases.RolloDefectuoso]
-        data['rollos C'] = raw[pclases.RolloC]
+        data['balas A+B'] = raw[pclases.Bala]['bultos']
+        data['balas C'] = raw[pclases.BalaCable]['bultos']
+        data['bigbag'] = raw[pclases.Bigbag]['bultos']
+        data['cajas'] = raw[pclases.Caja]['bultos']
+        data['pales'] = raw[pclases.Pale]['bultos']
+        data['rollos A'] = raw[pclases.Rollo]['bultos']
+        data['rollos B'] = raw[pclases.RolloDefectuoso]['bultos']
+        data['rollos C'] = raw[pclases.RolloC]['bultos']
+        # Datos para el gráfico de kg frabricados (producción)
+        data['kg fibra A+B'] = raw[pclases.Bala]['kg']
+        data['kg fibra C'] = raw[pclases.BalaCable]['kg']
+        data['kg fibra bigbag'] = raw[pclases.Bigbag]['kg']
+        data['kg fibra cemento'] = raw[pclases.Caja]['kg']
+        data['kg geotextiles A'] = raw[pclases.Rollo]['kg']
+        data['kg geotextiles B'] = raw[pclases.RolloDefectuoso]['kg']
+        data['kg geotextiles C'] = raw[pclases.RolloC]['kg']
         return data
 
     def _get_raw_data(self):
         data = {}
-        for clase in (pclases.Bala, pclases.BalaCable, pclases.Bigbag,
-                      pclases.Caja, pclases.Pale, pclases.Rollo,
-                      pclases.RolloDefectuoso, pclases.RolloC):
-            data[clase] = clase.select().count()
+        clases = (pclases.Bala, pclases.BalaCable, pclases.Bigbag,
+                  pclases.Caja, pclases.Pale, pclases.Rollo,
+                  pclases.RolloDefectuoso, pclases.RolloC)
+        for clase in clases:
+            data[clase] = dict()
+            data[clase]['bultos'] = clase.select().count()
+            for dim_name in ('pesobala', 'peso', 'pesobigbag'):
+                try:
+                    data[clase]['kg'] = clase.select().sum(dim_name)
+                except Exception:
+                    continue
+                else:
+                    break
         return data
