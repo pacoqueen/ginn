ba4a5788a40bf6656d782843cb7deba17ac80073
Francisco José Rodríguez Bogado <bogado@qinn.es>
2018-10-25 17:16:32 +0200

    Pruebas con rollos

--- ginn/api/netdata/deploy.sh
+++ ginn/api/netdata/deploy.sh
@@ -1,6 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
 # Coloca cada fichero en su sitio. Se ejecuta desde `bacall`.
+if [[ $EUID -ne 0 ]]; then
+   echo "Este _script_ debe ejecutarse como root: sudo "$0 1>&2
+   exit 1
+fi
 
 cp -f ginn.chart.py /usr/libexec/netdata/python.d/ginn.chart.py
 cp -f ginn.conf /etc/netdata/orig/python.d/ginn.conf
--- ginn/api/netdata/ginn.chart.py
+++ ginn/api/netdata/ginn.chart.py
@@ -7,17 +7,29 @@ from random import SystemRandom
 
 from bases.FrameworkServices.SimpleService import SimpleService
 
+import os, sys
+sys.path.append(os.path.join('/', 'home', 'compartido', 'ginn', 'ginn'))
+from framework import pclases
+
 # default module values
 # update_every = 4
 priority = 90000
 retries = 60
 
-ORDER = ['random']
-CHART = {
+ORDER = ['artículos', 'random']
+CHARTS = {
     'random': {
-        'options': [None, 'A random number', 'ordinal', 'random', 'random', 'line'],
+        'options': [None, 'A random number', 'ordinal', 'random', 'random',
+                    'line'],
         'lines': [
             ['random1']
+        ]
+    },
+    'artículos': {
+        'options': [None, 'Artículos fabricados', 'bultos', 'artículos',
+                    'producción', 'line'],
+        'lines': [
+            ['rollos']
         ]
     }
 }
@@ -27,7 +39,7 @@ class Service(SimpleService):
     def __init__(self, configuration=None, name=None):
         SimpleService.__init__(self, configuration=configuration, name=name)
         self.order = ORDER
-        self.definitions = CHART
+        self.definitions = CHARTS
         self.random = SystemRandom()
 
     @staticmethod
@@ -35,22 +47,22 @@ class Service(SimpleService):
         return True
 
     def get_data(self):
-        data = dict()
-
-        for i in range(1, 4):
-            dimension_id = ''.join(['random', str(i)])
-
-            if dimension_id not in self.charts['random']:
-                self.charts['random'].add_dimension([dimension_id])
-
-            raw = self._get_raw_data()
-            data[dimension_id] = raw
-
-        return data
+        return self._get_data()
 
     def _get_data(self):
-        return self.get_data()
+        data = dict()
+
+        # Datos para el gráfico de prueba (números aleatorios)
+        for i in range(1, 4):
+            dimension_id = ''.join(['random', str(i)])
+            if dimension_id not in self.charts['random']:
+                self.charts['random'].add_dimension([dimension_id])
+            data[dimension_id] = self.random.randint(0, 100)
+        # Datos para el gráfico de producción
+        raw = self._get_raw_data()
+        data['rollos'] = raw[pclases.Rollo]
+        return data
 
     def _get_raw_data(self):
-        data = self.random.randint(0, 100)
+        data = {pclases.Rollo: pclases.Rollo.select().count()}
         return data
