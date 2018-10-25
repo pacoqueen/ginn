7a501c1834a6654f991a22b3c0fade65e00310cc
Francisco José Rodríguez Bogado <bogado@qinn.es>
2018-10-25 17:24:18 +0200

    Mejoras en despliegue.

--- ginn/api/netdata/deploy.sh
+++ ginn/api/netdata/deploy.sh
@@ -5,7 +5,9 @@ if [[ $EUID -ne 0 ]]; then
    echo "Este _script_ debe ejecutarse como root: sudo "$0 1>&2
    exit 1
 fi
+BASEPATH=$(dirname $(realpath $0))
+exit 0
 
-cp -f ginn.chart.py /usr/libexec/netdata/python.d/ginn.chart.py
-cp -f ginn.conf /etc/netdata/orig/python.d/ginn.conf
-cp -f ginn.local.conf /etc/netdata/python.d/ginn.conf
+cp -f $BASEPATH/ginn.chart.py /usr/libexec/netdata/python.d/ginn.chart.py
+cp -f $BASEPATH/ginn.conf /etc/netdata/orig/python.d/ginn.conf
+cp -f $BASEPATH/ginn.local.conf /etc/netdata/python.d/ginn.conf
