d764315b722ea39dd78ec85145c54c279c321247
Francisco José Rodríguez Bogado <bogado@qinn.es>
2018-10-26 11:53:14 +0200

    Métricas a módulo independiente

--- ginn/framework/metrics/__init__.py
+++ ginn/framework/metrics/__init__.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from core import bultos_fabricados, produccion_estandar  # noqa
+from core import Bala, BalaCable, Bigbag, Caja, Pale    # noqa
+from core import Rollo, RolloDefectuoso, RolloC         # noqa
+from core import ParteDeProduccion                      # noqa
+from core import bultos_fabricados, produccion_estandar # noqa
 
 __all__ = ["bultos_fabricados", "produccion_estandar"]
