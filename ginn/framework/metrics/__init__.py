2fd75ae63368729655850f9a62face98d841b6ea
Francisco José Rodríguez Bogado <bogado@qinn.es>
2018-10-26 15:16:18 +0200

    Métricas almacén

--- ginn/framework/metrics/__init__.py
+++ ginn/framework/metrics/__init__.py
@@ -3,7 +3,7 @@
 
 from core import Bala, BalaCable, Bigbag, Caja, Pale    # noqa
 from core import Rollo, RolloDefectuoso, RolloC         # noqa
-from core import ParteDeProduccion                      # noqa
+from core import ParteDeProduccion, ProductoVenta       # noqa
 from core import bultos_fabricados, produccion_estandar, get_existencias # noqa
 
 __all__ = ["bultos_fabricados", "produccion_estandar"]
