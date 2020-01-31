d3a707380306f8693c9bad3c5fc3fff608901778
Francisco José Rodríguez Bogado <bogado@qinn.es>
2020-01-31 11:45:32 +0100

    Corregido error Decimal * float

--- ginn/api/tests/ramanujan.py
+++ ginn/api/tests/ramanujan.py
@@ -1761,13 +1761,11 @@ def build_fila_valoracion(fila, agrupacion):
     articulo = pclases.Articulo.get_articulo(codigo_trazabilidad)
     proyecto = murano.ops.get_proyecto(pv)
     precio = murano.ops.get_precio_coste(articulo)
+    precio = float(precio)  # Viene como Decimal de SQL
     # Malo sería que cambie el peso entre que genero el datalib de
     # desglose y monto este. Así me evito el error float*Decimal:
     peso_neto = articulo.peso_neto
-    try:
-        coste = precio * peso_neto
-    except TypeError:   # Viene como Decimal de SQL
-        coste = float(precio) * peso_neto
+    coste = precio * peso_neto
     # Asumimos la fecha del parte como fecha de fabricación de todos
     # los artículos. Se toma la fecha lógica del parte (si antes de las
     # 6:00, es del día anterior) no la fecha natural. Es con intención
