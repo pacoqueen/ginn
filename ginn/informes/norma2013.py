58e42a0b8d7468e796f5c275e4af60f4dbf16e95
Francisco José Rodríguez Bogado <bogado@qinn.es>
2020-06-23 14:24:36 +0200

    Quitamos temporalmente la segunda etiqueta para balas.

--- ginn/informes/norma2013.py
+++ ginn/informes/norma2013.py
@@ -1139,8 +1139,9 @@ def crear_etiquetas_balas(balas, mostrar_marcado=True, lang="es"):
         # print "Imprimiendo segunda etiquetas..."
         canvas.showPage()
         # Y ahora la etiqueta adicional por si se pierde la otra y para cargar.
-        create_etiqueta_backup_bala(canvas, bala)
-        canvas.showPage()
+        # XXX: TODO: FIXME: CWT: TEMP: Quitar la segunda etiqueta porque estamos sin stock de pegatinas.
+        # create_etiqueta_backup_bala(canvas, bala)
+        # canvas.showPage()
     canvas.save()
     return nomarchivo
 
